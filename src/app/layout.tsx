687126c1452c4f84762569d587a8909962848992
m-urculu <mrcel83@gmail.com>
2024-12-11 16:17:36 +0000

    removed "work in progress msg"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;800;900&display=swap" rel="stylesheet"></link>
       </head>
       <body className={syncopate.className}>
-        <div className="work-in-progress">
-          <p>WORK IN PROGRESS</p>
-        </div>
         {children}
       </body>
     </html>
