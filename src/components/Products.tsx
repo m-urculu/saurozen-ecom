e534f8bc49002e81e2a500a0d89490e7cfc2a691
m-urculu <mrcel83@gmail.com>
2025-02-03 08:38:10 +0000

    .

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,35 +9,20 @@ const Products = () => {
         <div className="arrivals-text">New Arrivals</div>
           <div aria-label="product-items" className="product-items">
           {productsData.products
-            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) 
+            .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()) 
             .slice(0, 4)
             .map(product => (
-            <button key={product.productId} className="sunglasses">
+              <button key={product.productId} className="sunglasses">
                 <div className="img-container">
-                <img src={product.images[0]} alt={product.model} className="product-img" />
+                  <img src={product.images[0]} alt={product.model} className="product-img" />
                 </div>
                 <div className="flex-col pt-[10px]">
                   <p className="collection-name">{product.collection}</p>
                   <p className="product-name">{product.model}</p>
                 </div>
-                <div className="flex items-center pt-[5px] space-x-[15px]">
-                  <p className="price">
-                    {product.discount && product.discount > 0
-                      ? `${Math.round(product.price * (1 - product.discount / 100))}€`
-                      : `${product.price}€`
-                    }
-                  </p>
-                  <p className={product.discount && product.discount > 0 ? "discounted-price" : "hidden"}>
-                    {product.discount && product.discount > 0 ? `${product.price}€` : null}
-                  </p>
-                  {product.discount && product.discount > 0 && (
-                    <div className="discount">
-                      <p className="percentage">-{product.discount}%</p>
-                    </div>
-                  )}
-                </div>    
-            </button>
-            ))}
+              </button>
+            ))
+          }
         </div>
         <div className="btn-container">
           <button className="view-sep">
